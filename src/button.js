42c6bf9b72735ab50674ade7e341f1c47ec55c40
Christian Deacon <gamemann@gflclan.com>
2020-11-13 04:10:08 +0000

    Add very simple HTML and Javascript project.

---
+++ src/button.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+    $('#btn').click(function()
+    {
+        // Add text to #fill.
+        $('#fill').html("This is additional text that should only show when you pressed the button aboved.");
+    });
+});
