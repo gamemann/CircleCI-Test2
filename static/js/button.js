7bd17c8cbe656f5cff30e594d350ff38eaf1e093
gamemann <gamemann@gflclan.com>
2020-11-16 05:40:57 +0000

    Correct grammer mistake.

--- static/js/button.js
+++ static/js/button.js
@@ -3,6 +3,6 @@ $(document).ready(function()
     $('#btn').click(function()
     {
         // Add text to #fill.
-        $('#fill').html("This is additional text that should only show when you pressed the button aboved.");
+        $('#fill').html("This is additional text that should only show when you pressed the button above.");
     });
 });
